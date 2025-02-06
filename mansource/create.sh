9753df211bdd8b0ece999bd9804c09deba5ae23f
Ryan Armstrong <Ryan Armstrong>
2025-02-05 19:40:58 -0500

    Add more Python manpages from help text

--- mansource/create.sh
+++ mansource/create.sh
@@ -17,7 +17,10 @@ txt2man -d "12 Sep 2015" -t aspectnes -r aspectnes-1.0 -s 1 -v "Ryan's Tools" $I
 txt2man -d "18 Jan 2018" -t aspecticon -r aspecticon-1.0 -s 1 -v "Ryan's Tools" $INDIR/aspecticon > $OUTDIR/aspecticon.1
 txt2man -d "12 Sep 2015" -t aspectsnes -r aspectsnes-1.0 -s 1 -v "Ryan's Tools" $INDIR/aspectsnes > $OUTDIR/aspectsnes.1
 help2man $BINDIR/imagegallery -n "Creates an HTML image gallery" -m "Ryan's Tools" -s 1 -N --version-string="1.0" > $OUTDIR/imagegallery.1
+help2man $BINDIR/thumbmaker -n "Creates thumbnails of images in a folder" -m "Ryan's Tools" -s 1 -N --version-string="1.0" > $OUTDIR/thumbmaker.1
 help2man $BINDIR/linkgallery -n "Creates a page of links to all files in a folder" -m "Ryan's Tools" -s 1 -N --version-string="1.0" > $OUTDIR/linkgallery.1
+help2man $BINDIR/imageshare -n "Shares images to a specified location" -m "Ryan's Tools" -s 1 -N --version-string="1.0" > $OUTDIR/imageshare.1
 help2man $BINDIR/manual-booklet -n "Creates a booklet from a series of individual pages" -m "Ryan's Tools" -s 1 -N --version-string="1.0" > $OUTDIR/manual-booklet.1
 help2man $BINDIR/manual-split -n "Splits a PDF document into individual pages" -m "Ryan's Tools" -s 1 -N --version-string="1.0" > $OUTDIR/manual-split.1
+help2man $BINDIR/manual-halfbinder -n "Creates half-letter page pairings from a series of individual pages" -m "Ryan's Tools" -s 1 -N --version-string="1.0" > $OUTDIR/manual-halfbinder.1
 txt2man -d "05 Aug 2012" -t halfpages -r halfpages-1.0 -s 1 -v "Ryan's Tools" $INDIR/halfpages > $OUTDIR/halfpages.1
